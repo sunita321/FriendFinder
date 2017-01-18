66448a44ff4fd6d5bafa6d9d04cc1b1b9b352647
Sunita Ramnarinesingh <sramnari@gmail.com>
2017-01-17 20:22:21 -0500

    Adding friends information to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+var friends = 
+[
+  {
+    "name":"Batman",
+    "photo":"https://idigitalcitizen.files.wordpress.com/2009/04/1batman.jpg",
+    "scores":[4, 5, 3, 2, 1, 4, 5, 3, 2, 1]
+  },
+
+  {
+    "name":"Superman",
+    "photo":"https://idigitalcitizen.files.wordpress.com/2009/04/1superman.jpg?w=180",
+    "scores":[4, 5, 5, 2, 3, 1, 3, 3, 2, 4] 
+
+  },
+
+  {
+    "name":"Spiderman",
+    "photo":"https://idigitalcitizen.files.wordpress.com/2009/04/1spiderman28.jpg",
+    "scores":[4, 2, 1, 5, 3, 3, 1, 5, 3, 2] 
+
+  },
+
+  {
+    "name":"Wonder Woman",
+    "photo":"https://images.alphacoders.com/709/thumb-350-709273.jpg",
+    "scores":[4, 2, 5, 2, 3, 5, 5, 3, 1, 3] 
+
+  },
+
+  {
+    "name":"Hulk",
+    "photo":"https://idigitalcitizen.files.wordpress.com/2009/04/1theincrediblehulk.jpg",
+    "scores":[2, 3, 2, 5, 5, 5, 5, 4, 3, 1] 
+
+  },
+
+  {
+    "name":"Flash",
+    "photo":"https://idigitalcitizen.files.wordpress.com/2009/04/1flash3-3.gif",
+    "scores":[2, 3, 2, 5, 5, 5, 5, 4, 3, 1] 
+
+  },
+
+  {
+    "name":"Joker",
+    "photo":"",
+    "scores":[5, 4, 3, 3, 4, 5, 1, 4, 2, 3]
+
+  },
+
+  {
+    "name":"Ironman",
+    "photo":"http://coolspotters.com/files/photos/422831/iron-man-profile.jpg",
+    "scores":[3, 4, 2, 3, 2, 4, 1, 2, 3, 3]
+
+  },
+
+  {
+    "name":"Wolverine",
+    "photo":"http://static3.comicvine.com/uploads/square_small/7/71081/1296684-hugh_jackman_wolverine_trailer.jpg",
+    "scores":[1, 2, 4, 3, 3, 5, 2, 3, 3, 4]
+
+  },
+
+  {
+    "name":"Thor",
+    "photo":"http://images.vcpost.com/data/images/full/51509/thor-3-ragnarok.jpeg",
+    "scores":[1, 1, 5, 4, 2, 4, 4, 3, 2, 4]
+
+  }
+
+];
